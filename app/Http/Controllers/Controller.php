45c0cfed88f0bb76f2528fd22f21c09f293c5528
Diego Pereira <san_diego19@hotmail.com>
2024-09-21 16:50:30 -0300

    feat(trait): implementando ControllerTrait com métodos CRUD reutilizáveis

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
+use App\Traits\ControllerTrait;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ *@OA\Info(
+ * title="Criasol Api - Referência da API",
+ * version="1.0.0",
+ * description="Documentação de cada EndPoint para integração.",
+ * @OA\Contact(
+ *  email="dhipereira21@gmail.com"
+ * ),
+ *),
+ */
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ControllerTrait;
 }
